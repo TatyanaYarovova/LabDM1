b99ded97a232cf19013988b5f8981b22fa530474
Tatyana <tatyana.yarovova@yandex.ru>
2019-03-22 18:25:10 +0400

    Lab12

--- index.php
+++ index.php
@@ -17,16 +17,67 @@
 						if(isset($_POST["sub"]))
 						{
 							echo '<br>';
-							
+						
 							$mass1 = split(" ", $_POST["mass1"]);
-							
 							$mass2 = split(" ", $_POST["mass2"]);
-							$result = array_merge($mass1, $mass2);
+
+
+
+							if($mass1[1] % 2 != 0){
+								echo "Первый элемент  массиве 1 должен быть четным";
+								echo'<br>';
+							}
+							if($mass1[2] % 2 == 0){
+								echo "Второй элемент в массиве 1 должен быть нечетным";
+								echo'<br>';
+							}
+							if($mass1[3] % 2 == 0){
+								echo "Третий элемент в массиве 1 должен быть нечетным ";
+								echo'<br>';
+							}
+							if($mass1[4] % 2 != 0){
+								echo "Четвертый элемент в массиве 1 должен быть четным";
+								echo'<br>';
+							}
+							
+							if($mass2[1] % 2 != 0){
+								echo "Первый элемент  массиве 2 должен быть четным";
+								echo'<br>';
+							}
+							if($mass2[2] % 2 == 0){
+								echo "Второй элемент в массиве 2 должен быть нечетным";
+								echo'<br>';
+							}
+							if($mass2[3] % 2 == 0){
+								echo "Третий элемент в массиве 2 должен быть нечетным ";
+								echo'<br>';
+							}
+							if($mass2[4] % 2 != 0){
+								echo "Четвертый элемент в массиве 2 должен быть четным";
+								echo'<br>';
+							}
+							
+							$result = $mass1 + $mass2;
 							//print_r($result);
+							echo "Объединение:"; 
+							echo '<br>';
 							foreach($result as $value){
 								echo $value, " ";
 							}
-							
+							echo '<br>';
+							echo "Пересечение:";
+							echo '<br>';
+							$per = array_intersect($mass1, $mass2); 
+							foreach($per as $value){
+								echo $value, " ";
+							} 
+							echo '<br>';
+							echo "Симметрическая разность:";
+							echo '<br>';
+							$difference = array_merge(array_diff($mass1 , $mass2) , array_diff($mass2 , $mass1));
+							foreach($difference as $value){
+								echo $value, " ";
+							} 
 						}
 				?>
 		</div>
